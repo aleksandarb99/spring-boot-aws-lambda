aa50dc7d0667fd1f7397212e15ace17e97551710
Aleksandar <abuljevic8@gmail.com>
2022-12-05 15:08:12 +0100

    Just started

---
+++ src/main/java/com/levi9/springbootawslambda/SpringBootAwsLambdaApplication.java
@@ -0,0 +1,14 @@
+package com.levi9.springbootawslambda;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsLambdaApplication.class, args);
+	}
+
+}
